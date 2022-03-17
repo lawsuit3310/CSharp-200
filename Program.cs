94107ac594e89d931d48f8f046e060837676efe9
lawsuit3310 <aimeet5480@gmail.com>
2022-03-17 21:59:22 +0900

    string.format에 대하여

--- Program.cs
+++ Program.cs
@@ -12,13 +12,15 @@ namespace C_____200제.예제_풀이
             Ex005 ex5 = new Ex005();
             Ex006 ex6 = new Ex006();
             Ex007 ex7 = new Ex007();
+            Ex008 ex8 = new Ex008();
 
             //ex1.Run();
             //ex3.Run();
             //ex4.Run();
             //ex5.Run();
             //ex6.Run();
-            ex7.Run();
+            //ex7.Run();
+            ex8.Run();
         }
     }
 }
---
+++ 예제 풀이/Ex008.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_____200제.예제_풀이
+{
+    class Ex008
+    {
+        public void Run()
+        {
+            Console.WriteLine("10 이하의 소수: {0},{1},{2},{3}",2,3,5,7);
+
+            string primes = string.Format("10 이하의 소수: {0},{1},{2},{3}", 2, 3, 5, 7);
+            Console.WriteLine(primes); 
+        }
+    }
+}

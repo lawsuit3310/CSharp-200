201c2964ea3b49445d528548cdcbbcdc79ce7aed
lawsuit3310 <aimeet5480@gmail.com>
2022-03-17 21:55:02 +0900

    여러 자료형들과 Console.Write, Console.WriteLine의 차이에 대해

---
+++ 예제 풀이/Ex007.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_____200제.예제_풀이
+{
+    class Ex007
+    {
+        public void Run()
+        {
+            bool b = true;
+            char c = 'A'; //char 형은 작은 따옴표, string 형은 큰 따옴표
+            decimal d = 1.234m; //m은 decimal 형에 붙이는 접미사
+            double e = 1.23456789;
+            float f = 1.23456789f; //f는 float 형에 붙이는 접미사
+            int i = 1234;
+            string s = "Hello";
+
+            Console.WriteLine(b);
+            Console.Write(c); //Console.Write는 줄 바꿈 없이, Console.WriteLine 내용이 끝날 때마다 줄 바꿈
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(i);
+            Console.WriteLine(s);
+        }
+    }
+}

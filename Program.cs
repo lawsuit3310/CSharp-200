7394d15b74093b4c721c726038eaadbe2b37b21c
lawsuit3310 <aimeet5480@gmail.com>
2022-03-19 23:17:12 +0900

    연산 순서

--- Program.cs
+++ Program.cs
@@ -15,11 +15,12 @@ namespace C_____200제.예제_풀이
             Ex008 ex8 = new Ex008();
             Ex009 ex9 = new Ex009();
             Ex010 ex10 = new Ex010();
-            Ex011 ex11= new Ex011();
-            Ex012 ex12= new Ex012();
-            Ex013 ex13= new Ex013();
-            Ex014 ex14= new Ex014();
-            Ex015 ex15= new Ex015();
+            Ex011 ex11 = new Ex011();
+            Ex012 ex12 = new Ex012();
+            Ex013 ex13 = new Ex013();
+            Ex014 ex14 = new Ex014();
+            Ex015 ex15 = new Ex015();
+            Ex016 ex16 = new Ex016();
 
             //ex1.Run();
             //ex3.Run();
@@ -34,7 +35,8 @@ namespace C_____200제.예제_풀이
             //ex12.Run();
             //ex13.Run();
             //ex14.Run();
-            ex15.Run();
+            //ex15.Run();
+            ex16.Run();
         }
     }
 }
---
+++ 예제 풀이/Ex016.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_____200제.예제_풀이
+{
+    class Ex016
+    {
+        public void Run()
+        {
+            Console.WriteLine(3+4*5);
+            Console.WriteLine((3+4)*5);
+            Console.WriteLine(3*4/5);
+            Console.WriteLine(4/5*3);
+
+            int a = 10, b = 20, c;
+            Console.WriteLine(c=a+b);
+        }
+    }
+}

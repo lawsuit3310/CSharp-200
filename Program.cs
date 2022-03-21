4c0e108a6c0eda163de0536e4fff0e8e7c773381
lawsuit3310 <aimeet5480@gmail.com>
2022-03-21 22:12:04 +0900

    DivideByZeroException에 대해

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ namespace C_____200제.예제_풀이
             Ex015 ex15 = new Ex015();
             Ex016 ex16 = new Ex016();
             Ex017 ex17 = new Ex017();
+            Ex018 ex18 = new Ex018();
 
             //ex1.Run();
             //ex3.Run();
@@ -37,7 +38,9 @@ namespace C_____200제.예제_풀이
             //ex13.Run();
             //ex14.Run();
             //ex15.Run();
-            ex17.Run();
+            //ex16.Run();
+            //ex17.Run();
+            ex18.Run();
         }
     }
 }
---
+++ 예제 풀이/Ex018.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_____200제.예제_풀이
+{
+    class Ex018
+    {
+        public void Run()
+        {
+            int x = 10, y = 0;
+
+            try
+            {
+                Console.WriteLine(x/y);
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
